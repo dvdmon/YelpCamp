a4b5f0e3340b0cd12f2671d31c53e3a000112b4f
Levi Wallach <dvdmon@users.noreply.github.com>
2021-12-06 22:40:43 -0500

    add user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongooose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+// adds on a username, a passport, makes sure usernames are unique, etc.: 
+UserSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User', UserSchema);
