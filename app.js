471a36ed66e8f862e0fbaad88b5c5ff1bd1987c5
Levi Wallach <dvdmon@users.noreply.github.com>
2021-12-08 15:44:57 -0500

    add registration form, and routes for form and post

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const User = require('./models/user');
 
 
 // require routes
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 const db = mongoose.connection;
@@ -68,8 +69,9 @@ app.use((req, res, next) => {
 
 
 // use routes
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
 
 
 
